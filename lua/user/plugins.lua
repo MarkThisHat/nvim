c3e5fec5ffdca61e61db30118a3d7529944ce7ab
Marcos <marcos.chiu@gmail.com>
2024-07-25 20:53:29 -0300

    fix: broken configs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -6,7 +6,10 @@ require('lazy').setup({
     'kyazdani42/nvim-tree.lua',
     'neovim/nvim-lspconfig',
     'nvim-treesitter/nvim-treesitter',
-    'akinsho/bufferline.nvim',
+    {
+        'akinsho/bufferline.nvim',
+        requires = 'nvim-tree/nvim-web-devicons'
+    },
     'folke/which-key.nvim',
     'goolord/alpha-nvim',
 })

b35e8365dae0b097445468a04d43e92c11c7ca12
Marcos <marcos.chiu@gmail.com>
2024-08-04 18:28:15 -0300

    decent description achieved

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,35 +1,36 @@
 -- lua/user/keymaps.lua
-local opts = { noremap = true, silent = true }
+local function opts(desc)
+  return { noremap = true, silent = true, desc = desc }
+end
 
 local keymap = vim.api.nvim_set_keymap
 
 -- LSP keybindings
-keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts("Find Files"))
 
-keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)            -- Hover information
-keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)      -- Go to definition
-keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)     -- Go to declaration
-keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)  -- Go to implementation
-keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)      -- List references
-keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)  -- Rename
-keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts) -- Code action
+keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts("Hover Information"))
+keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts("Go to Definition"))
+keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts("Go to Declaration"))
+keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts("Go to Implementation"))
+keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts("List References"))
+keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts("Rename"))
+keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts("Code Action"))
 
 -- Optional: Diagnostic keybindings
-keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)    -- Go to previous diagnostic
-keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)    -- Go to next diagnostic
-keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts) -- Set location list
+keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts("Previous Diagnostic"))
+keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts("Next Diagnostic"))
+keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts("Set Location List"))
 
--- Treesitter keybinding for syntax tree inspection
-keymap("n", "<leader>ts", ":Inspect<CR>", opts)  -- Highlight captures under cursor
-keymap("n", "<leader>tp", ":InspectTree<CR>", opts)  -- Toggle Treesitter playground
-keymap("n", "<leader>te", ":EditQuery<CR>", opts)  -- Open the Live Query Editor
+-- Treesitter keybindings for syntax tree inspection
+keymap("n", "<leader>ts", ":Inspect<CR>", opts("Highlight Captures Under Cursor"))
+keymap("n", "<leader>tp", ":InspectTree<CR>", opts("Toggle Treesitter Playground"))
+keymap("n", "<leader>te", ":EditQuery<CR>", opts("Open Live Query Editor"))
 
--- Iron keymaps (also has a list of commands, see :h iron-commands for all available commands)
-keymap("n", "<leader>rs", "<cmd>IronRepl<cr>", opts)
-keymap("n", "<leader>rr", "<cmd>IronRestart<cr>", opts)
-keymap("n", "<leader>rF", "<cmd>IronFocus<cr>", opts)
-keymap("n", "<leader>rh", "<cmd>IronHide<cr>", opts)
+-- Iron keymaps
+keymap("n", "<leader>rs", "<cmd>IronRepl<cr>", opts("Start IronRepl"))
+keymap("n", "<leader>rr", "<cmd>IronRestart<cr>", opts("Restart IronRepl"))
+keymap("n", "<leader>rF", "<cmd>IronFocus<cr>", opts("Focus IronRepl"))
+keymap("n", "<leader>rh", "<cmd>IronHide<cr>", opts("Hide IronRepl"))
 
 -- Removals
-keymap("n", "gc", "", opts)
-keymap("n", "<space>r", "", opts)
+keymap("n", "gc", "", opts("Remove Keybinding"))
